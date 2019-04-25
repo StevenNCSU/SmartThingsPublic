569cc7cd534c5edeeaa3413f550051d5a9c4049c
Steven Stewart <bearcave805@gmail.com>
2019-04-25 15:13:13 -0400

    Initial Upload

---
+++ smartapps/stevenncsu/parent/stevens-smart-lighting.src/stevens-smart-lighting.groovy
@@ -0,0 +1,54 @@
+/**
+ *  Steven Smart Lights
+ *
+ *  Copyright 2019 Steven Stewart
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Steven's Smart Lighting",
+    namespace: "StevenNCSU/parent",
+    author: "Steven Stewart",
+    description: "Replicates SmartThings Smart Lights functions with a few custom features like ability to disable a lighting automation",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    // The parent app preferences are pretty simple: just use the app input for the child app.
+    page(name: "mainPage", title: "Automations", install: true, uninstall: true,submitOnChange: true) {
+        section {
+            app(name: "smartLightAutomation", appName: "Steven's Smart Lights", namespace: "StevenNCSU/automations", title: "Create New Lighting Automation", multiple: true)
+            }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // nothing needed here, since the child apps will handle preferences/subscriptions
+    // this just logs some messages for demo/information purposes
+    log.debug "there are ${childApps.size()} child smartapps"
+    childApps.each {child ->
+        log.debug "child app: ${child.label}"
+    }
+}
